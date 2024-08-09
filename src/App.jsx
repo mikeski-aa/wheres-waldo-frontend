6dcbda8b0eab3957d697e9f765fe605e6ba76c8f
mikeski-aa <159564491+mikeski-aa@users.noreply.github.com>
2024-08-09 14:32:36 +0200

    Working on dropdown menu

--- src/App.jsx
+++ src/App.jsx
@@ -3,9 +3,14 @@ import wheresWaldo from "../src/assets/925901.jpg";
 import Nav from "./components/Nav";
 import "./App.css";
 
+// when a coordiante is selected, it needs to be compared with the general hitbox
+// hitbox should be 15x15 pixels for initial testing, to be increased in case the user experience is not good.
+// if the hitbox is too small, user experience will suffer
+
 function App() {
   const [coordinates, setCoordinate] = useState([0, 0]);
 
+  // on click gets coordinates of click
   const showCoord = (e) => {
     const xcoord = e.nativeEvent.offsetX;
     const ycoord = e.nativeEvent.offsetY;
@@ -16,7 +21,11 @@ function App() {
   return (
     <>
       <div className="mainContent">
-        <Nav />
+        <div className="header">
+          <div>LOGO</div>
+          <Nav />
+        </div>
+
         <div className="gameArea">
           <img
             src={wheresWaldo}
