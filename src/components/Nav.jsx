915e01c36674b69682cd6debf3d56d0ee352861f
mikeski-aa <159564491+mikeski-aa@users.noreply.github.com>
2024-08-09 13:17:51 +0200

    Setting up the app

---
+++ src/components/Nav.jsx
@@ -0,0 +1,15 @@
+import "../styles/nav.css";
+
+function Nav() {
+  return (
+    <>
+      <div className="navBarContainer">
+        <div className="optionOne">Game one</div>
+        <div className="optionTwo">Game two</div>
+        <div className="optionThree">Game three</div>
+      </div>
+    </>
+  );
+}
+
+export default Nav;
